8b5adb65993b5b47c4f6eef6c01ebcc422de6b2e
krkyk <100661771+krkyk@users.noreply.github.com>
2023-04-03 22:39:50 +0900

    変数について

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
-import "./styles.css";
+/**
+ * const, let　変数宣言
+ */
+let str = "let変数";
+console.log(str);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// letは上書き可能
+str = "let変数上書き";
+console.log(str);
+
+// letは再宣言不可
+// let str = "let変数を再宣言";
+// console.log(str);
+
+const str2 = "const変数";
+console.log(str2);
+
+// constは上書き不可
+// str2 = "const変数を上書き";
+
+// constを再宣言
+// const str2 = "const変数を再宣言";
+// console.log(str2);
+
+// オブジェクトではプロパティの変更可能
+const obj = {
+  name: "太郎",
+  age: 12
+};
+
+obj.name = "はるか";
+obj.address = "Hiroshima";
+console.log(obj);
+
+// 配列の中身の変更可能
+const array = ["dog", "cat"];
+array[0] = "rabbit";
+array.push("bird");
+console.log(array);
