78b77e4fc6c646bc421472d3c50c637cc7888d48
krkyk <100661771+krkyk@users.noreply.github.com>
2023-04-24 14:15:02 +0900

    ボタン・結果表示デザイン調整

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./styles.css";
 
 export default function App() {
   const [leftValue, setLeftValue] = useState(0);
@@ -15,7 +16,9 @@ export default function App() {
   };
 
   const clickOperator = (ope) => {
-    setOperator(ope);
+    if (!leftValue !== 0) {
+      setOperator(ope);
+    }
   };
 
   const clickEqual = () => {
@@ -41,28 +44,38 @@ export default function App() {
   };
 
   return (
-    <>
-      <div>計算結果：{answer}</div>
-      <div className="num-btn">
-        <button onClick={() => clickNumber(0)}>0</button>
-        <button onClick={() => clickNumber(1)}>1</button>
-        <button onClick={() => clickNumber(2)}>2</button>
-        <button onClick={() => clickNumber(3)}>3</button>
-        <button onClick={() => clickNumber(4)}>4</button>
-        <button onClick={() => clickNumber(5)}>5</button>
-        <button onClick={() => clickNumber(6)}>6</button>
-        <button onClick={() => clickNumber(7)}>7</button>
-        <button onClick={() => clickNumber(8)}>8</button>
+    <div class="container">
+      <div className="ans">
+        {leftValue}
+        {operator === null || operator}
+        {operator === null || rightValue}
+        {answer === 0 || "="}
+        {answer === 0 || answer}
+      </div>
+      <div>
         <button onClick={() => clickNumber(9)}>9</button>
-      </div>
-      <div className="ope-btn">
+        <button onClick={() => clickNumber(8)}>8</button>
+        <button onClick={() => clickNumber(7)}>7</button>
         <button onClick={() => clickOperator("+")}>+</button>
-        <button onClick={() => clickOperator("-")}>-</button>
-        <button onClick={() => clickOperator("/")}>÷</button>
-        <button onClick={() => clickOperator("*")}>x</button>
-        <button onClick={() => clickEqual()}>=</button>
       </div>
-      <button onClick={() => clickClear()}>c</button>
-    </>
+      <div>
+        <button onClick={() => clickNumber(6)}>6</button>
+        <button onClick={() => clickNumber(5)}>5</button>
+        <button onClick={() => clickNumber(4)}>4</button>
+        <button onClick={() => clickOperator("-")}>-</button>
+      </div>
+      <div>
+        <button onClick={() => clickNumber(3)}>3</button>
+        <button onClick={() => clickNumber(2)}>2</button>
+        <button onClick={() => clickNumber(1)}>1</button>
+        <button onClick={() => clickOperator("*")}>x</button>
+      </div>
+      <div>
+        <button onClick={() => clickNumber(0)}>0</button>
+        <button onClick={() => clickClear()}>C</button>
+        <button onClick={() => clickEqual()}>=</button>
+        <button onClick={() => clickOperator("/")}>÷</button>
+      </div>
+    </div>
   );
 }
